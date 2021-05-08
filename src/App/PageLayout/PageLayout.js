297df56e6eb57cc8013b6f7cc16e5a860fba41ca
peter-oms <peter@oxfordmedicalsimulation.com>
2021-05-08 16:18:45 +0100

    Add material-ui and create an app theme

--- src/App/PageLayout/PageLayout.js
+++ src/App/PageLayout/PageLayout.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import { CssBaseline } from '@material-ui/core'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
 import { Route } from 'Router'
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+})
+
 const PageLayout = ({ children }) => {
-  return <Route />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Route />
+    </ThemeProvider>
+  )
 }
 
 export default PageLayout
--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import { Box, Typography } from '@material-ui/core'
+
 const Home = () => {
-  return <div>Hello world</div>
+  return (
+    <Box mx="auto" maxWidth={800}>
+      <Typography>Hello world</Typography>
+    </Box>
+  )
 }
 
 export default Home
